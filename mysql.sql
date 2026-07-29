33eb47d2a67344c0f4d31d889379a7446aff37f0
Zaid Ahmad Nalband <zaidnalband0412@gmail.com>
2026-07-29 11:23:53 +0530

    BANKING

---
+++ mysql.sql
@@ -0,0 +1,20 @@
+create database banking_07;
+use banking_07;
+CREATE TABLE Customers
+(
+    CustomerID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15)
+);
+drop table Customers;
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+select * from Customers;
