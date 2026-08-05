c67e1319b06142cbdab94d954b5aa69bcafec84e
Zaid Ahmad Nalband <zaidnalband0412@gmail.com>
2026-08-05 12:58:48 +0530

    BANKINGDB

--- mysql.sql
+++ mysql.sql
@@ -9,6 +9,7 @@ CREATE TABLE Customers
     Phone VARCHAR(15)
 );
 drop table Customers;
+
 CREATE TABLE Customers (
     CustomerID INT PRIMARY KEY,
     FirstName VARCHAR(50),
@@ -17,4 +18,95 @@ CREATE TABLE Customers (
     Phone VARCHAR(15),
     AccountCreationDate DATE
 );
+
+alter table Customers
+add person varchar(50);
+
 select * from Customers;
+
+insert into Customers(CustomerID, FirstName, LastName, Email, Phone, AccountCreationDate, person)
+values (001,"Zaid","Nalband","z@gmail.com",9423219315,"2026-07-06","zaid");
+
+alter table Customers
+drop person;
+
+alter table Customers
+rename column Phone TO phonenumber;
+
+alter table Customers
+modify phonenumber bigint;
+
+alter table Customers
+add constraint chk_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
